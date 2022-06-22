7b1f49766f579a8bea0a4ade31998c13c43276b7
iamjooon2 <iamjooon2@gmail.com>
2022-06-22 20:18:58 +0900

    set index.js

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 const express = require('express');
 
-const app = express();
+const indexRoute = require('./routes/index.js');
+const userRoute = require('./routes/user.js');
 
+const PORT = 3000;
+
+const server = () => {
+    const app = express();
+
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
+
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+
+    userRoute(app);
+    indexRoute(app);
+
+    app.listen(PORT, () => {
+        console.log(`server is now listening to ${PORT}`);
+    });
+}
+
+try {
+    server(PORT);
+} catch (e) {
+    return e;
+};
